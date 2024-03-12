f9daa0ee72006567bd1a1f56169b598f3f26c28c
VinChriZ <107181042+VinChriZ@users.noreply.github.com>
2024-03-12 08:14:36 +0700

    Add files via upload

---
+++ c14210205/public/src/js/app.js
@@ -0,0 +1,32 @@
+
+var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
+
+function showDetails(id) {
+  document.getElementById(id).style.display = 'block';
+}
+
+function hideDetails(id) {
+  document.getElementById(id).style.display = 'none';
+}
